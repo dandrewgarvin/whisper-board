1fa94d76ab3694dd8a1d7fb678d16262e169df67
Andrew Garvin <me@andrewgarv.in>
2020-05-09 15:55:41 -0600

    remove console logs

--- src/controllers/SocketController.js
+++ src/controllers/SocketController.js
@@ -4,23 +4,16 @@ import config from "../config/config.json";
 class SocketController {
   constructor(store) {
     this.socket = io.connect(config.backend_url);
-    console.log("this.socket", this.socket);
-    console.log("config.backend_url", config.backend_url);
-
-    console.log("store", store);
 
     this.socket.on("moved", (payload) => {
-      console.log("moved payload", payload);
       store.moveToken(payload);
     });
 
     this.socket.on("deleted", (payload) => {
-      console.log("deleted payload", payload);
       store.removeFromGrid(payload);
     });
 
     this.socket.on("resized", (payload) => {
-      console.log("resized payload", payload);
       store.resizeToken(payload);
     });
   }
